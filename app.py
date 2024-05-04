33085420c31a2af628c91dbd16d0143f6a806144
carloodq <dallaquerciacarlo@gmail.com>
2024-05-04 13:11:12 +0200

    search

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
+from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+import os
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 import streamlit as st
 import pandas as pd
 from datetime import datetime
 import time
-from tabs import calendario, upload_docs, search_docs
+from tabs import calendario, upload_docs, search_docs, substitute, orari_profs, cerca_circ
+
 
 st.title("App Gestionale Liceo Medi")
 
 # Create the tabs
-tab1, tab2, tab3 = st.tabs(["Calendario", "Carica Circolari", "Ricerca Circolari"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Calendario", "Carica Circolari", "Ricerca Circolari", "Ricerca supplemente", "Orari", "Ricerca"])
 
 # Content for each tab
 with tab1:
   calendario()
-
 
 with tab2:
    upload_docs()
@@ -20,7 +25,12 @@ with tab2:
 with tab3:
    search_docs()
 
+with tab4:
+   substitute()
 
+with tab5:
+   orari_profs()
 
-
+with tab6:
+   cerca_circ()
 
